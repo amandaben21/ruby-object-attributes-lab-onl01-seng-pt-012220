188cf944678c7727770098d439d62cbe14d337a3
Amanda Contreras <amandaben21@aol.com>
2020-02-09 05:27:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,37 +1,21 @@
 class Dog
-  def intialize (name)
-    @name=name
-  end
-  
+
   def name
     @name
   end
-end
-snoopy = Dog.new
-snoopy.instance_variable_get(:@name) 
 
-def name
-  snoopy= Dog.new
-  snoopy.name = "Snoopy"
-  snoopy.instance_variable_set(:@name ,"Snoopy")
+
+def name=(new_name)
+  @name = new_name
+  
   end
 
-class Dog
 
-  def initalize(breed)
-  @breed= breed
+  def breed=(new_breed)
+  @breed= new_breed
  end
 
  def breed
   @breed
- end
 end
-
-begale = Dog.new
-begale.instance_variable_get(:@breed) 
-
-def name
-  begale = Dog.new
-  begale.breed = "Begale"
-  begale.instance_variable_set(:@breed , "Begale")
-  end
+end
